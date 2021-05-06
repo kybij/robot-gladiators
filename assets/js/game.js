9a26e2ade7bf5c19c1813ff2d141d4595f6ddc6a
Kylee Bijerring <kylee24b@gmail.com>
2021-05-06 17:34:57 -0600

    saving highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -150,15 +150,27 @@ var fight = function (enemy) {
 
 /* END GAME FUNCTION */
   var endGame = function() {
-  // if a player is still alive, player wins!
-    if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    }
-    else {
-      window.alert("You've lost your robot in battle.");
-    }
+      window.alert("The game has now ended. Let's see how you did!");
 
-    var playAgainConfirm = window.confirm("Would you like to play again?");
+      //check localStorage for high score, if it's not there, use 0
+      var highScore = localStorage.getItem("highscore");
+      if (highScore === null) {
+        highScore = 0;
+      }
+
+    // if player has more money than the high score, player has new high score
+    if (playerInfo.money > highScore) {
+      localStorage.setItem("highschore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
+
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+      }
+      else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Mayebe next time!");
+      }
+
+      //ask player if they'd like to play again
+      var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
       //restart the game
